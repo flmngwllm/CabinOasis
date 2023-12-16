3516cdde956b86edfb45735bb570ee84f0327e07
flmngwllm <flmngwllm232@gmail.com>
2023-12-16 17:31:07 -0500

    added checkin/out buttons, errorboundaries in App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 // import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      onReset={() => window.location.replace("/")}
+      FallbackComponent={ErrorFallback}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
